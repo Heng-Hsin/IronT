2e5b81a821377f9d6f16a207ce4978b5c74b6cf6
Heng-Hsin <heng.hsin.chang@gmail.com>
2018-11-30 23:17:33 -0800

    Initial commit

---
+++ src/main/java/com/grab/Main.java
@@ -0,0 +1,11 @@
+package com.grab;
+
+/**
+ * Created by polar
+ * 11/30/2018 23:12
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world");
+  }
+}
